c69490f803d4bc0086e0070b41993eba05875edd
adi94958 <117360640+adi94958@users.noreply.github.com>
2023-11-19 11:50:34 +0700

    Add files via upload

---
+++ PemesananMk/src/main/java/com/mycompany/pemesananmk/PemesananMk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pemesananmk;
+
+/**
+ *
+ * @author gamin
+ */
+public class PemesananMk {
+
+    public static void main(String[] args) {
+        System.out.println("Hsssd!");
+    }
+}
